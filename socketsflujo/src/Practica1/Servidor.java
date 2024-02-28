ec59577f4236a57afe2d662ade8438f26c536a6e
Christian Rodriguez <160700378+chrizesynegros@users.noreply.github.com>
2024-02-28 14:36:20 -0600

    Update Servidor.java

--- socketsflujo/src/Practica1/Servidor.java
+++ socketsflujo/src/Practica1/Servidor.java
@@ -12,7 +12,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 /*
- * nombre: Mario Alonso Alvarez Perez
+ * nombre: Christian Ulises Rodriguez Noriega
  * tema del programa: Practica 1
  * descripcion: cliente que separa mensaje puetro e ip
  * fecha: 1/febrero/2019
