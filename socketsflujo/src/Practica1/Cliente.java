6a695fdf4e41c8b535c6938c4bacfead5c7d65be
Christian Rodriguez <160700378+chrizesynegros@users.noreply.github.com>
2024-02-28 14:33:50 -0600

    Update Cliente.java

--- socketsflujo/src/Practica1/Cliente.java
+++ socketsflujo/src/Practica1/Cliente.java
@@ -12,7 +12,7 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 
 /*
- * nombre: Mario Alonso Alvarez Perez
+ * nombre: Christian Ulises Rodriguez Noriega
  * tema del programa: Practica 1
  * descripcion: cliente que separa mensaje puetro e ip
  * fecha: 1/febrero/2019
